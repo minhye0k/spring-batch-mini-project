673a4c1ecbd60d3ff5090b8dd5402d318791c78b
minhyeok <omh34658981@gmail.com>
2023-06-25 20:28:36 +0900

    study spring batch

---
+++ src/main/java/mini/project/springbatch/SpringBatchApplication.java
@@ -0,0 +1,16 @@
+package mini.project.springbatch;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@EnableBatchProcessing
+@SpringBootApplication
+public class SpringBatchApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBatchApplication.class, args);
+    }
+
+}
